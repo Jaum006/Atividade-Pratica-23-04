988dbec92aeb06d2e53cce15288dcc071e8d9648
João Marcos <joaomariano.dev@gmail.com>
2026-04-23 19:36:05 -0300

    Update final tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Outlet, Link } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>Clínica Sorriso Saudável</h1>
+      <nav style={{ marginBottom: "20px" }}>
+        <Link to="/">Cadastro</Link> |{" "}
+        <Link to="/resumo">Resumo</Link>
+      </nav>
+      <Outlet />
     </div>
   );
 }
-
 export default App;
